83e6e91b71684e0da6b36f4f6b1e625718a9a527
BHAIRAVI-sal <bhairavi3155@gmail.com>
2026-02-18 07:45:01 +0530

    adding ass1 file

--- Bizmetric.py
+++
@@ -1,7 +0,0 @@
-echo "# Bizmetric_Bhairavi" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/BHAIRAVI-sal/Bizmetric_Bhairavi.git
-git push -u origin main
